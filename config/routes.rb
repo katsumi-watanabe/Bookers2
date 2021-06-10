1e42a2968446ad4217ccd6f880d3d8a23b2327fc
katsumi-watanabe <ktm981124@gmail.com>
2021-06-10 00:42:36 +0000

    seconds commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   devise_for :users
+  get "homes/about" ,to: "homes#about"
   resources :user, only: [:index, :show, :edit, :update]
   resources :book, only: [:index, :show, :edit, :create, :update, :destroy]
 end
