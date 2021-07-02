e29f75b02f11dd5f17140847822b8f5a265c02c0
katsumi-watanabe <ktm981124@gmail.com>
2021-07-02 06:17:12 +0000

    いいねまで済み

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'favorites/create'
+  get 'favorites/destroy'
   root to: 'home#top'
   devise_for :users
   get "home/about" ,to: "home#about"
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
+  resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
