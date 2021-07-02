5c0bc3c14d577d1af33580f4bd3a811306a7070e
katsumi-watanabe <ktm981124@gmail.com>
2021-07-02 15:30:24 +0000

    follow-end

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'post_comments/create'
-  get 'post_comments/destroy'
-  get 'favorites/create'
-  get 'favorites/destroy'
   root to: 'home#top'
   devise_for :users
   get "home/about" ,to: "home#about"
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
