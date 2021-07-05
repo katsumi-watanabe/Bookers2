984511863c633fc85680ecb322e432931df1ca4f
katsumi-watanabe <ktm981124@gmail.com>
2021-07-05 15:38:35 +0000

    star-3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_120728) do
+ActiveRecord::Schema.define(version: 2021_07_05_151358) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_07_02_120728) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "evaluation"
   end
 
   create_table "favorites", force: :cascade do |t|
